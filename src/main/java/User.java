1b5654307669e3aec394ecf3e1ddf724e1382096
michaelcoopman <macoopman@gmail.com>
2019-07-25 16:46:15 -0700

    Added User class w/ data, noargs, allargs, builder annotations

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     private String firstName;
